e948ba7bd9c2506252da546e9dd4410b2caef7d4
ZwerOxotnik <zweroxotnik@gmail.com>
2021-11-05 11:18:26 +0300

    Add Info.lua

---
+++ Info.lua
@@ -0,0 +1,12 @@
+-- https://api.cuberite.org/InfoFile.html
+g_PluginInfo =
+{
+	Name = "CuberiteExamplePlugin",
+	Date = "2021-11-05",
+	Description = "[Unofficial][WIP] This is an example plugin to get you comfortable with Cuberite plugins etc",
+	AdditionalInfo = {},
+	Commands = {},
+	ConsoleCommands = {},
+	Permissions = {},
+	Categories = {},
+}
