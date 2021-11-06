033ec5c284b5f71e890a122fb4b23d94a0a03624
ZwerOxotnik <zweroxotnik@gmail.com>
2021-11-06 17:13:43 +0300

    Refactor

--- main.lua
+++ main.lua
@@ -3,6 +3,8 @@
 -- Warning: I'm not a developer of Cuberite and not familiar with the API confidently etc.
 -- Nevertheless, I hope these examples can help to newcomers etc. Perhaps, there are some issues, so let people know about it.
 
+local root = cRoot:Get() -- https://api.cuberite.org/cRoot.html
+
 -- The most important stuff here (https://api.cuberite.org/Writing-a-Cuberite-plugin.html)
 -- function Initialize is called on plugin startup. It is the place where the plugin is set up.
 function Initialize(Plugin)
@@ -41,8 +43,6 @@ end
 
 ---@param player table # https://api.cuberite.org/cPlayer.html
 function OnPlayerJoined(player)
-  local root = cRoot:Get() -- https://api.cuberite.org/cRoot.html
-
   -- Safely for the server teleport a joined player to center
   root:GetDefaultWorld():QueueTask(function()
     root:DoWithPlayerByUUID(player:GetUUID(), teleportPlayerToCenter)
